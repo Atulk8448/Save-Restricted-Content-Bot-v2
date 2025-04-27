35624ddf2a195b5664252e4a0b7726e557f9f9d2
Atulk8448 <atulk8448@gmail.com>
2025-04-27 11:26:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,31 @@
-# devgagan
-# Note if you are trying to deploy on vps then directly fill values in ("")
+# Copyright (c) 2025 devgagan : https://github.com/devgaganin.
+# Licensed under the GNU General Public License v3.0.
+# See LICENSE file in the repository root for full license text.
 
-from os import getenv
+# No dotenv needed because we're filling values manually
 
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
+# VPS --- FILL COOKIES 🍪 in """ ... """
 
 INST_COOKIES = """
-# wtite up here insta cookies
+# write up here insta cookies
 """
 
 YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "23237831"))
-API_HASH = getenv("API_HASH", "017dc661ca1432ac2fe5ecb62499d88f")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6103594386").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002421745690"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
-STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+API_ID = 26414373
+API_HASH = "70383671d550e23a1acd930c2a3754b2"
+BOT_TOKEN = "7719266620:AAEvY-xJfyzr7BHrVeoqiDMGlkSCOPcmEKA"
+MONGO_DB = "mongodb+srv://atulk8448:2FwbOtqBUf8KKYEx@cluster0.qh31ho4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+OWNER_ID = [5568385998]  # put inside list
+DB_NAME = "telegram_downloader"  # default
+STRING = None  # optional
+LOG_GROUP = -1002683561710  # your log group
+FORCE_SUB = -1002669385784  # used your channel id
+MASTER_KEY = "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq"  # default from your code
+IV_KEY = "s7Yx5CpVmE3F"  # default from your code
+YT_COOKIES = YTUB_COOKIES
+INSTA_COOKIES = INST_COOKIES
+FREEMIUM_LIMIT = 0
+PREMIUM_LIMIT = 50000
